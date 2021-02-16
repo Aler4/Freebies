d451de434e0ffbf969f3aa266730e05e7a0c5a38
unknown <alerch5666@gmail.com>
2021-02-16 19:55:54 +0200

    owl

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// for (var i = 0; i < 101; i++) {
+// 	if (i % 3 == 0 ) {
+// 		console.log('FIZZ');
+// 	};
+// 	if (i % 5 == 0 && i % 3 != 0) {
+// 		console.log('buzz');
+// 	};
+// 	if (i % 3 == 0 && i % 5 == 0) {
+// 		console.log('fizzbuzz');
+// 	}
+// 	console.log(i);
+// }
+// let size = 8;
+// let board = "";
+
+// for (let y = 0; y < size; y++) {
+// 	for(let x = 0; x < size; x++) {
+// 		if ((x + y) % 2 == 0) {
+// 			board += "+";
+// 		}
+// 		else {
+// 			board += "#";
+// 		}
+// 	}
+// 	board += "\n";
+// }
+// console.log(board);
---
+++ js/owl.carousel.js
@@ -0,0 +1,20 @@
+$(function() {
+	$('.owl-carousel').owlCarousel({
+		loop:4,	//Бесконечный цыкл слайдов
+		margin:10, //Margin справа в пикс
+		nav:true,	//Кнопки переключения
+		navText:['&#10132;','&#10132;'],	
+		dots:true, 
+		responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2.5
+            },
+            1000:{
+                items:2.5
+            }
+        } //Адаптив отображение количествo слайдов
+	});
+});
